5c9f938145df5683771f65d28e72bee85fbf7ec5
pimpmypc89 <pimpmypc.guru@gmail.com>
2016-03-16 13:13:52 -0700

    Fixed out of bounds problem on Khan Exercise

--- Exercises/Development/MapReduceEx.js
+++ Exercises/Development/MapReduceEx.js
@@ -8,11 +8,15 @@
       
     // Initialise the exercise
       initArr: function(arr_size) {
-	  var phrase2 = "my saab has stopped running and the engine is now smoking from a fire"
+	  var phrase3 = "the cat in the hat is very fat and it sat on a bat and now it is on the mat"
+	  var phrase2 = "my saab has stopped running and the engine is now smoking from a fire that is bad"
 	  var phrase = "a brown dog jumped over the white dog who caught a frisbie after chasing the cat"
 	  var phrase;
-	  if(Math.random() >= 0.5)
+	  var quickpick = Math.random();
+	  if(quickpick <= 0.25)
 	      phrase = phrase2;
+	  if(quickpick >= 0.6)
+	      phrase = phrase3;
 	  var phrase_array = phrase.split(" ");
 	  var i;
 	  var next_val;
@@ -44,11 +48,10 @@
            	  maxCount = modeMap[element];
               }
     	  }
-	  
 	  if(maxElements.length > 1)
 	  {
-	      frequent = maxElements[Math.floor(Math.random() * (maxElements.length - 0 + 1)) + 0];
-	      var insert = Math.floor(Math.random() * (my_array.length - 0 + 1) + 0);
+	      frequent = maxElements[Math.floor(Math.random() * ((maxElements.length -1) - 0 + 1)) + 0];		
+	      var insert = Math.floor(Math.random() * ((my_array.length-1) - 0 + 1) + 0);
 	      var temp = my_array[insert];
 	      my_array[insert] = frequent;
 	      my_array.push(temp);
@@ -59,7 +62,6 @@
 	  for (i = 1; i < arr_size; i++) {
 	      array_str = array_str + "[" + my_array[i] + " : 1] <br>";
 	  }
-	  console.log(frequent);
 	  return array_str;
       },
       
